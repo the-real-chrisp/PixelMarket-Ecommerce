40b11197ed1365be4b1f877286c5ea69a5d963d3
Chris Purcell <chrisp0000@gmail.com>
2023-12-19 20:43:07 -0800

    Added category_name to Category model.

--- models/Category.js
+++ models/Category.js
@@ -12,7 +12,10 @@ Category.init(
       primaryKey: true,
       autoIncrement: true
     },
-    
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
