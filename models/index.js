f92900295c7f0c8a89f3aaf84e2bfe4ca50c9238
Chris Purcell <chrisp0000@gmail.com>
2023-12-29 15:11:49 -0800

    Added associations on models/index.js

--- models/index.js
+++ models/index.js
@@ -1,19 +1,26 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
 
-// Categories have many Products
-
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: { ProductTag }
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: { ProductTag }
+})
 
 module.exports = {
   Product,
